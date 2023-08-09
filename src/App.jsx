dae87221f5cc821bb45de845c8f91a2bae8a5b20
mamun-mahmood <mamamun1999@gmail.com>
2023-08-10 03:10:50 +0600

    project initialized

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Landing from "./pages/Landing";
+import { Box } from "@mui/material";
+
+function App() {
+  return (
+    <Box>
+      <Landing />
+    </Box>
+  );
+}
+
+export default App;
